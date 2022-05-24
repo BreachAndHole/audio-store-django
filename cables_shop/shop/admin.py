ea63fc0cf8db09077332f7537de4ee8f4b17fca9
Sergey Frolov <frolovsu25@gmail.com>
2022-05-24 23:02:12 +0300

    CableType model crated

--- cables_shop/shop/admin.py
+++ cables_shop/shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CableTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'name_plural', 'description', 'slug')
+    list_display_links = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(CableType, CableTypeAdmin)
--- cables_shop/shop/models.py
+++ cables_shop/shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class CableType(models.Model):
+    name = models.CharField('Название (ед.ч)', max_length=30, unique=True)
+    name_plural = models.CharField('Название (мн.ч)', max_length=50)
+    slug = models.SlugField('URL')
+    description = models.TextField('Описание', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Тип кабеля'
+        verbose_name_plural = 'Типы кабелей'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return f'#{self.pk} {self.name}'
