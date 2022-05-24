32f866d0c9bbc3d535647844a7599d8070e7f966
Sergey Frolov <frolovsu25@gmail.com>
2022-05-24 22:51:20 +0300

    first views added

--- cables_shop/shop/views.py
+++ cables_shop/shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'shop/index.html', context)
+
+
+def all_cables(request):
+    context = {
+        'title': 'Товары',
+    }
+    return render(request, 'shop/all_cables.html', context)
