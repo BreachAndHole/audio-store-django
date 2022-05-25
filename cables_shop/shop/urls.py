4da418fd1f28afae4bb7f75b9d10711e629f9c96
BreachAndHole <100375207+BreachAndHole@users.noreply.github.com>
2022-05-25 13:52:48 +0300

    cable page render added

--- cables_shop/shop/urls.py
+++ cables_shop/shop/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('cables/', views.all_cables, name='all_cables'),
+    path('cable/<slug:cable_slug>', views.cable_page, name='cable'),
 ]
--- cables_shop/shop/views.py
+++ cables_shop/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import *
 
 
@@ -17,3 +18,12 @@ def all_cables(request):
         'cables': Cable.objects.all(),
     }
     return render(request, 'shop/all_cables.html', context)
+
+
+def cable_page(request, cable_slug):
+    cable = get_object_or_404(Cable, slug=cable_slug)
+    context = {
+        'title': f'Страница товара - {cable.name}',
+        'cable': cable,
+    }
+    return render(request, 'shop/cable.html', context)
