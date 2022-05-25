cc437c1331faac3e867cdf21385385d4e49610f4
BreachAndHole <100375207+BreachAndHole@users.noreply.github.com>
2022-05-25 13:24:25 +0300

    All cables page dropdown cable_types filter added

--- cables_shop/cables_shop/settings.py
+++ cables_shop/cables_shop/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -16,7 +15,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -26,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cables_shop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -73,7 +70,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -93,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -104,7 +99,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- cables_shop/shop/views.py
+++ cables_shop/shop/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from .models import *
 
+
 def index(request):
     context = {
         'title': 'Главная страница',
         'cable_types': CableType.objects.all(),
     }
-
-    print(CableType.objects.get(pk=1).photo.url)
-
     return render(request, 'shop/index.html', context)
 
 
 def all_cables(request):
     context = {
         'title': 'Товары',
+        'cable_types': CableType.objects.all(),
+        'cables': Cable.objects.all(),
     }
     return render(request, 'shop/all_cables.html', context)
