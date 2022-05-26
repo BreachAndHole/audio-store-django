b2eec382e4fa72a9b9b1dcd52962ea73a6b3803c
BreachAndHole <100375207+BreachAndHole@users.noreply.github.com>
2022-05-26 14:40:08 +0300

    Title cable photo now displaying on all cables page

--- cables_shop/shop/templatetags/shop_tags.py
+++ cables_shop/shop/templatetags/shop_tags.py
@@ -2,3 +2,9 @@ from django import template
 from shop.models import *
 
 register = template.Library()
+
+
+@register.simple_tag(name='get_title_photo')
+def get_title_photo(cable: Cable):
+    print(cable)
+    return CablePhoto.objects.get(is_title=True, cable=cable)
