7afa7d5a987d78d6542a968ce842337fae692eb1
Sergey Frolov <frolovsu25@gmail.com>
2022-06-02 21:22:15 +0300

    Custom errors implemented

---
+++ cables_shop/shop/errors.py
@@ -0,0 +1,6 @@
+class JSONResponseParsingError(Exception):
+    """Error during parsing JSON response with update cart data"""
+
+
+class UpdateCartError(Exception):
+    """Error during updating cart"""
