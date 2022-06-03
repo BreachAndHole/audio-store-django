e6bc5f2aeaeb24f3f9e1347e59510aab6643fec8
Sergey Frolov <frolovsu25@gmail.com>
2022-06-03 22:46:28 +0300

    django test initial

--- cables_shop/shop/tests.py
+++ cables_shop/shop/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class URLTests(TestCase):
+    def test_index_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_cables_page_url(self):
+        response = self.client.get('/cables/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_url(self):
+        response = self.client.get('/user/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page_url(self):
+        response = self.client.get('/user/registration/')
+        self.assertEqual(response.status_code, 200)
